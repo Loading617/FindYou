341c9424725d38f9e90841c0edab1bab7adf6f18
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-04-10 06:15:24 -0400

    Delete message.swift

--- message.swift
+++
@@ -1,8 +0,0 @@
-func sendMessage(chatID: String, senderID: String, text: String) {
-    let db = Firestore.firestore()
-    db.collection("Chats").document(chatID).updateData([
-        "messages": FieldValue.arrayUnion([
-            ["senderID": senderID, "text": text, "timestamp": Date()]
-        ])
-    ])
-}
